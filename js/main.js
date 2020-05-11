ff35cc515bfbe4eba8db25163ac493886bc8415a
MJMoquin <mjmoquin@gmail.com>
2020-05-11 10:09:17 -0500

    add card files

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/*------------ constants ---------------*/
+const players = {
+    'player': {
+        name: '',
+        bank: 0
+    }
+    'dealer': {
+        name: 'Dealer',
+        bank: null
+    }
+}
+
+/*------ app's state (variables) -------*/
+
+/*----- cached element references ------*/
+
+/*---------- event listeners -----------*/
+
+/*------------- functions --------------*/
