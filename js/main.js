3d42bdb419d06771438344bdbd7bef0b59ad94a8
MJMoquin <mjmoquin@gmail.com>
2020-05-13 12:25:06 -0500

    finish game play

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ const players = {
 
 
 /*------ app's state (variables) -------*/
+let turn = 1;
 let pot = 0;
 let deck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"];
 let gameDeck = [];
@@ -29,7 +30,7 @@ const standBtn = document.getElementById('standBtn');
 
 /*---------- event listeners -----------*/
 standBtn.addEventListener('click', stand);
-
+hitBtn.addEventListener('click', hit)
 
 /*------------- functions --------------*/
 init();
@@ -40,32 +41,9 @@ function init() {
     dealCards();
     renderHands();
 }
-function renderScore(total, player) {
-    if (player === 2) {
-        playScoreEl.innerHTML = playScoreEl.innerHTML + Number(total);
-    }
-    else {
-        dealScoreEl.innerHTML = dealScoreEl.innerHTML + Number(total);
-    }
-}
+
  
-function renderHands() {
-    for (let i in players) {
-        for (let j = 0; j < players[i].hand.length; j++) {
-            let idStart;
-            if(i == 1) {
-                idStart = 'dc';
-            }
-            else {
-                idStart = 'pc';
-            }
-            myId = idStart + j;
-            myElement = document.getElementById(myId)
-            myElement.classList.add(players[i].hand[j]);
-        }
-    }
-    totalHand(2);
-}
+
 
 function shuffleDeck () {
     let cardToShuffle;
@@ -90,7 +68,6 @@ function dealCards() {
         gameDeck.shift();
       }   
     }
-    
 }
 
 function totalHand(player) {
@@ -109,26 +86,100 @@ function totalHand(player) {
         total += Number(marker);
       }
     }
-    if (hand.includes('hA') || hand.includes('dA') || hand.includes('cA') || hand.includes('sA') && total <= 11) {
-      total += 10;
-    }
-    renderScore(total, player);   
+    if (total <= 11) {
+        if (hand.includes('hA') || hand.includes('dA') || hand.includes('cA') || hand.includes('sA')) {
+        total += 10;
+        }
+    }   
+    renderScore(total, player);
+    
 }
 
 function hit() {
-    totalHand(playerIndex)
+    if (turn != 1) return;
+    players[2].hand.push(gameDeck[0]);
+    gameDeck.shift();
+    totalHand(2);
+    renderHands();
 }
 
 function stand() {
+    if (turn != 1) return;
+    turn *= -1;
     dealerPlay();
 }
 
 function dealerPlay () {
     document.getElementById('dc0').classList.remove('back-red');
-    document.getElementById('dealerScore').classList.remove('hidden');
+    document.getElementById('dealerTotal').classList.remove('hidden');
+    while ((Number(dealerScore.innerText)) < 17 && (playerScore.innerText <= 21)) {
+        players[1].hand.push(gameDeck[0]);
+        gameDeck.shift();
+        totalHand(1);
+        renderHands()
+    }
     totalHand(1);
+    renderHands();
+    // getWinner();
 }
 
 function getWinner() {
+    let winner;
+    if (playerScore.innerText <= 21 && dealerScore.innerText <= 21) {
+        if (Number(playerScore.innerText) > Number(dealerScore.innerText)) {
+            winner = 'Player'
+        }
+        else if (Number(playerScore.innerText) < Number(dealerScore.innerText)) {
+            winner = 'Dealer'
+        }
+        else {
+            winner = 'T'
+        }
+    }
+    else if (Number(playerScore.innerText <= 21) && Number(dealerScore.innerText > 21)) {
+        winner = 'Player'
+    }
+    else {
+        winner = 'Dealer'
+    }
+    declareWinner(winner);
+}
 
+function declareWinner(winner) {
+    if (winner !== 'T') {
+        alert(`${winner} Wns!!`)
+    }
+    else {
+        alert("It's a Tie.")
+    }
+}
+
+function renderHands() {
+    for (let i in players) {
+        for (let j = 0; j < players[i].hand.length; j++) {
+            let idStart;
+            if(i == 1) {
+                idStart = 'dc';
+            }
+            else {
+                idStart = 'pc';
+            }
+            myId = idStart + j;
+            myElement = document.getElementById(myId);
+            myElement.classList.add(players[i].hand[j]);
+            myElement.classList.remove('hidden');
+        }
+    }
+    totalHand(2);
+    totalHand(1);
+}
+
+function renderScore(total, player) {
+    if (player === 2) {
+        playScoreEl.innerText = Number(total);
+        if (Number(playerScore.innerText) >= 21) stand();
+    }
+    else {
+        dealScoreEl.innerText = Number(total);
+    }
 }
