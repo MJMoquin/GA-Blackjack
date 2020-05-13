385df33d4b588daf3730ff834f37ac194f187238
MJMoquin <mjmoquin@gmail.com>
2020-05-13 16:41:23 -0500

    fix game reset

--- js/main.js
+++ js/main.js
@@ -22,17 +22,24 @@ let gameDeck = [];
 /*----- cached element references ------*/
 const dealerCards = document.getElementById('dealerHand');
 const playerCards = document.getElementById('playerHand');
-const cardId = document.getElementById('')
 const playScoreEl = document.getElementById('playerScore');
 const dealScoreEl = document.getElementById('dealerScore');
 const hitBtn = document.getElementById('hitBtn');
 const standBtn = document.getElementById('standBtn');
 const resetBtn = document.getElementById('reset');
+const dealTotal = document.getElementById('dealerTotal');
+const playTotal = document.getElementById('playerTotal');
 
 /*---------- event listeners -----------*/
 standBtn.addEventListener('click', stand);
 hitBtn.addEventListener('click', hit)
-resetBtn.addEventListener('click', init);
+resetBtn.addEventListener('click', function() {
+    players[1].hand = [];
+    players[2].hand = [];
+    resetBoard();
+    turn =1;
+    init();
+});
 
 /*------------- functions --------------*/
 init();
@@ -42,6 +49,36 @@ function init() {
     shuffleDeck();
     dealCards();
     renderHands();
+    
+}
+
+function resetBoard() {
+    for (let i in players) {
+        for (let j = 0; j <= 6; j++) {
+            let idStart;
+            if(i == 1) {
+                idStart = 'dc';
+            }
+            else {
+                idStart = 'pc';
+            }
+            myId = idStart + j;
+            myElement = document.getElementById(myId);
+            classList = myElement.classList;
+            while (classList.length > 0) {
+                classList.remove(classList.item(0));
+            }
+            classList.add('card', 'large', 'hidden')
+            if (j < 2) classList.remove('hidden');
+            if (myId === 'dc0') classList.add('back-red');
+        }
+    }
+    // playTotal.innerText = "Total: "
+    // dealTotal.innerText = "Total: "
+    dealTotal.classList.add('hidden');
+    resetBtn.classList.add('hidden')
+    hitBtn.classList.remove('hidden')
+    standBtn.classList.remove('hidden')
 }
 
 function shuffleDeck () {
@@ -111,7 +148,7 @@ function stand() {
 function dealerPlay () {
     document.getElementById('dc0').classList.remove('back-red');
     document.getElementById('dealerTotal').classList.remove('hidden');
-    while ((Number(dealerScore.innerText)) < 17 && (playerScore.innerText <= 21)) {
+    while (dealTotal.innerText != "BUSTED!!!" &&(Number(dealScoreEl.innerText)) < 17 && (playScoreEl.innerText <= 21)) {
         players[1].hand.push(gameDeck[0]);
         gameDeck.shift();
         totalHand(1);
@@ -119,38 +156,10 @@ function dealerPlay () {
     }
     totalHand(1);
     renderHands();
-    // getWinner();
-}
-
-function getWinner() {
-    let winner;
-    if (playerScore.innerText <= 21 && dealerScore.innerText <= 21) {
-        if (Number(playerScore.innerText) > Number(dealerScore.innerText)) {
-            winner = 'Player'
-        }
-        else if (Number(playerScore.innerText) < Number(dealerScore.innerText)) {
-            winner = 'Dealer'
-        }
-        else {
-            winner = 'T'
-        }
-    }
-    else if (Number(playerScore.innerText <= 21) && Number(dealerScore.innerText > 21)) {
-        winner = 'Player'
-    }
-    else {
-        winner = 'Dealer'
-    }
-    declareWinner(winner);
-}
-
-function declareWinner(winner) {
-    if (winner !== 'T') {
-        alert(`${winner} Wns!!`)
-    }
-    else {
-        alert("It's a Tie.")
-    }
+    hitBtn.classList.add('hidden')
+    standBtn.classList.add('hidden')
+    resetBtn.classList.remove('hidden')
+    
 }
 
 function renderHands() {
@@ -175,10 +184,49 @@ function renderHands() {
 
 function renderScore(total, player) {
     if (player === 2) {
+        if (Number(total) >=21) stand();
         playScoreEl.innerText = Number(total);
-        if (Number(playerScore.innerText) >= 21) stand();
     }
     else {
         dealScoreEl.innerText = Number(total);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+// function getWinner() {
+//    et wiElnner;
+//     if (playerSco.inneElrText <= 21 && dealerScore.innerText <= 21) //   El      if (Number(playerSco.inneElrText) > Number(dealerScore.innerText)) {
+//             winner = 'Player'
+//         }
+//      El else if (Number(playerSco.inneElrText) < Number(dealerScore.innerText)) {
+//             winner = 'Dealer'
+//         }
+//         else {
+//             winner = 'T'
+//         }
+//     }/    El else if (Number(playerSco.inneElrText <= 21) && Number(dealerScore.innerText > 21)) {
+//         winner = 'Player'
+//     }
+//     else {
+//         winner = 'Dealer'
+//     }
+//     declareWinner(winner);
+// }
+
+// function declareWinner(winner) {
+//     if (winner !== 'T') {
+//         alert(`${winner} Wns!!`)
+//     }
+//     else {
+//         alert("It's a Tie.")
+//     }
+// }
